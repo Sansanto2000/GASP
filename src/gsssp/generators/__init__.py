e5fb2c96b3481c5877db7fc7bc6b1770a58e590a
Santiago Ponte <santiagoandresponteahon@hotmail.com>
2026-09-02 15:18:22 -0300

    feat: [ID-005] :sparkles: generador de recortes de observaciones

--- src/gsssp/generators/__init__.py
+++ src/gsssp/generators/__init__.py
@@ -1,5 +1,6 @@
 """Generadores de lotes etiquetados para frameworks de aprendizaje automatico."""
 
+from .observationCropSequence import ObservationCropSequence
 from .spectrumLabeledSequence import OutputFormat, SpectrumLabeledSequence
 
-__all__ = ["OutputFormat", "SpectrumLabeledSequence"]
+__all__ = ["ObservationCropSequence", "OutputFormat", "SpectrumLabeledSequence"]
